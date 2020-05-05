960f783f5fcf349c9bdf25adc933a6cc4047e2ce
nikhil.dhore92@gmail.com <nikhil.dhore92@gmail.com>
2020-05-05 18:44:50 +0530

    added unit tes

---
+++ IBONikhil/IBO.UnitTests/BaseUnitTest.cs
@@ -0,0 +1,13 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBO.UnitTests
+{
+    public abstract class BaseUnitTest
+    {
+        public MockRepository MockBaseRepository { get; private set; }
+
+    }
+}
