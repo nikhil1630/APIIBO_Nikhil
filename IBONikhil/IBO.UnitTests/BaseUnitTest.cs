d7cbe0514e2b770f9df7b42dc51965afd45ffdfe
nikhil.dhore92@gmail.com <nikhil.dhore92@gmail.com>
2020-05-07 13:20:27 +0530

    Added Unit test

---
+++ IBONikhil/IBO.UnitTests/BaseUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Ploeh.AutoFixture;
+
+namespace IBO.UnitTests
+{
+    public abstract class BaseUnitTest
+    {
+        public MockRepository MockBaseRepository { get; private set; }
+        public virtual Fixture TestFixture { get; private set; }
+
+        protected BaseUnitTest()
+        {
+            MockBaseRepository = new MockRepository(MockBehavior.Strict);
+            TestFixture = new Fixture();
+        }
+    }
+}
---
+++ IBONikhil/IBO.UnitTests/Services/StudentServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using IBO.Business;
+using IBO.Business.DTOs;
+using IBO.IBusiness;
+using IBO.IRepository;
+using IBO.Repository.Entities;
+using Moq;
+using Ploeh.AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IBO.UnitTests.Services
+{
+    public class StudentServiceTest : BaseUnitTest
+    {
+        private readonly Mock<IStudentService> _studentService;
+        private readonly Mock<IStudentRepository> _studentRepository;
+        private readonly Mock<ILoggerRepository> _loggerRepository;
+
+        private IStudentService _target;
+        public StudentServiceTest()
+        {
+            _studentService = MockBaseRepository.Create<IStudentService>();
+            _studentRepository = MockBaseRepository.Create<IStudentRepository>();
+            _loggerRepository = MockBaseRepository.Create<ILoggerRepository>();
+            _target = new StudentService(_studentRepository.Object, _loggerRepository.Object);
+
+        }
+        [Fact]
+        public void GetStudentDetails()
+        {
+            var studentBusiness = TestFixture.Create<Task<List<StudentDTOs>>>();
+            _studentRepository.Setup(x => x.GetAllStudentDetails()).Returns(studentBusiness);
+
+            var result = _target.GetAllStudentDetails();
+
+            MockBaseRepository.VerifyAll();
+            Assert.NotNull(result);
+        }
+
+    }
+}
