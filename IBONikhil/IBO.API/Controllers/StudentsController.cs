79cada039769f7d919b022701eb5b5084afbfa61
nikhil.dhore92@gmail.com <nikhil.dhore92@gmail.com>
2020-05-11 00:09:44 +0530

    Added API Versioning

--- IBONikhil/IBO.API/Controllers/StudentsController.cs
+++ IBONikhil/IBO.API/Controllers/StudentsController.cs
@@ -15,7 +15,10 @@ using Microsoft.Extensions.Logging;
 
 namespace IBO.API.Controllers
 {
-    [Route("api/[controller]")]
+    //[Route("api/[controller]")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
     [ApiController]
     public class StudentsController : ControllerBase
     {
@@ -29,10 +32,9 @@ namespace IBO.API.Controllers
             _logger = logger;
             _studentValidator = studentValidator;
 
-
         }
-
-        [HttpGet]
+       
+        [HttpGet, MapToApiVersion("2.0"),MapToApiVersion("1.0")]
         public async Task<List<StudentDTOs>> GetAllStudent()
         {
             try
@@ -47,61 +49,108 @@ namespace IBO.API.Controllers
             }
         }
 
-        [HttpGet]
+        
+        [HttpGet, MapToApiVersion("1.0")]
         [Route("GetAllStudentName")]
         public async Task<string> GetAllStudentName()
         {
-            var studentsByName = await _studentService.GetAllStudentName();
-            return studentsByName.ToString();
+            try
+            {
+                var studentsByName = await _studentService.GetAllStudentName();
+                return studentsByName.ToString();
+            }
 
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get all student name data", ex);
+                return null;
+            }
         }
 
         [HttpGet]
         [Route("GetStudentById/{id}")]
         public async Task<StudentDTOs> GetStudentById(int? id)
         {
-            if (id.HasValue)
+            try
             {
-                var studentById = await _studentService.GetStudentByID(id);
-                return studentById;
+                if (id.HasValue)
+                {
+                    var studentById = await _studentService.GetStudentByID(id);
+                    return studentById;
+                }
+                return null;
             }
-            return null;
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get all student by {id} ", ex);
+                return null;
+            }
+
         }
 
+        [ApiVersion("2.0")]
         [HttpPost("register")]
         //[Authorize(Roles ="Admin")]
         public async Task<IActionResult> Register([FromBody] StudentDTOs studentDTOs)
         {
-            if(_studentValidator.CanPostSchool(studentDTOs))
+            try
             {
-                if (!ModelState.IsValid)
-                    return BadRequest();
-                await _studentService.Register(studentDTOs);
-                return Ok("Student with name = " + studentDTOs.FirstName + " " + studentDTOs.LastName + " Registered successfully.");
+                if (_studentValidator.CanPostSchool(studentDTOs))
+                {
+                    if (!ModelState.IsValid)
+                        return BadRequest();
+                    await _studentService.Register(studentDTOs);
+                    return Ok("Student with name = " + studentDTOs.FirstName + " " + studentDTOs.LastName + " Registered successfully.");
+                }
+                return BadRequest("User access denied");
             }
-            return BadRequest("User access denied");
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to register student", ex);
+                return BadRequest();
+            }
+
         }
 
-
+        [ApiVersion("2.0")]
         [HttpDelete]
         public async Task<IActionResult> Delete(int? id)
         {
-            var deletdStudent = await _studentService.DeleteStudentById(id);
-            if (deletdStudent)
-                return Ok("Student with Id = " + id.ToString() + " deleted successfully.");
-            return BadRequest("Student with Id = " + id.ToString() + " not found to delete");
+            try
+            {
+                var deletdStudent = await _studentService.DeleteStudentById(id);
+                if (deletdStudent)
+                    return Ok("Student with Id = " + id.ToString() + " deleted successfully.");
+                return BadRequest("Student with Id = " + id.ToString() + " not found to delete");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to register student", ex);
+                return BadRequest();
+            }
         }
 
+
+        [ApiVersion("2.0")]
         [HttpPut]
 
         public async Task<IActionResult> Update(int id, [FromBody] StudentDTOs studentDTOs)
         {
-            if (!ModelState.IsValid)
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest();
+                var updateStudent = await _studentService.Update(id, studentDTOs);
+                if (updateStudent)
+                    return Ok("Student with ID = " + id.ToString() + " Updated successfully.");
+                return BadRequest("Student with Id = " + id.ToString() + " not found to update");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to register student", ex);
                 return BadRequest();
-            var updateStudent= await _studentService.Update(id, studentDTOs);
-            if(updateStudent)
-            return Ok("Student with ID = " + id.ToString() + " Updated successfully.");
-            return BadRequest("Student with Id = " + id.ToString() + " not found to update");
+            }
+
         }
     }
 }
--- IBONikhil/IBO.API/Startup.cs
+++ IBONikhil/IBO.API/Startup.cs
@@ -13,6 +13,9 @@ using Microsoft.Extensions.Hosting;
 using Swashbuckle.AspNetCore;
 using Microsoft.OpenApi.Models;
 using IBO.API.Validator;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 
 namespace IBO.API
 {
@@ -30,7 +33,7 @@ namespace IBO.API
         {
             services.AddControllers();
             services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddStackExchangeRedisCache(x => x.Configuration =Configuration.GetConnectionString("Redis"));
+            services.AddStackExchangeRedisCache(x => x.Configuration = Configuration.GetConnectionString("Redis"));
             services.AddScoped<IStudentService, StudentService>();
             services.AddScoped<IStudentRepository, StudentRepository>();
             services.AddScoped<ILoggerService, LoggerService>();
@@ -40,7 +43,8 @@ namespace IBO.API
             services.AddScoped<IStudentValidator, StudentValidator>();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v2", new OpenApiInfo {
+                c.SwaggerDoc("v2", new OpenApiInfo
+                {
                     Title = "IBO API",
                     Version = "v2"
                 });
@@ -52,33 +56,41 @@ namespace IBO.API
                     .AllowAnyMethod();
 
             }));
+            services.AddApiVersioning();
 
+            services.AddApiVersioning(builder =>
+                 {
+                     builder.AssumeDefaultVersionWhenUnspecified = true;
+                     builder.DefaultApiVersion = new ApiVersion(new DateTime(2016, 7, 1));
+                 });
+
+            services.AddApiVersioning(o => o.ApiVersionReader = new HeaderApiVersionReader("api-version"));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseCors("MyPolicy");
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v2/swagger.json", "My API V2");
-            });
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseDeveloperExceptionPage();
         }
+
+        app.UseHttpsRedirection();
+        app.UseCors("MyPolicy");
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v2/swagger.json", "My API V2");
+        });
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
     }
+}
 }
